f76447e2a713f7813e3a8eca18195fc130c29706
lichtgarage <44371122+lichtgarage@users.noreply.github.com>
2018-10-24 15:15:14 +0200

    Add files via upload

---
+++ Homework22102018.py
@@ -0,0 +1,9 @@
+secret = int(24)
+guess = int (raw_input("What's your guess?"))
+
+if guess == secret:
+
+    print "Congratulations! You guessed correctly!"
+
+else:
+    print "Sorry, you are wrong!"
